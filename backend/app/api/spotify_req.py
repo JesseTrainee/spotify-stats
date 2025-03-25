77f87c882da4fe72695a66f26bbf583f963f2c37
Jesse Soares <jesse.soares@medportal.com.br>
2025-03-24 22:44:40 -0300

    remove unused files and code

--- backend/app/api/spotify_req.py
+++ backend/app/api/spotify_req.py
@@ -1,7 +1,7 @@
 import base64
 from fastapi import APIRouter
 import httpx
-from app.core.config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from core.config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 
 router = APIRouter()
 BASE_URL = "https://api.spotify.com/v1"
@@ -14,11 +14,11 @@ async def get_top_tracks():
         return response.json()
 
 @router.post("/login")
-def login(username, password):
+async def login(username: str, password: str):
     bytes_uft8 = f"{username}:{password}".encode("utf-8")
     base_64 = base64.b64encode(bytes_uft8).decode("utf-8")
 
-    with httpx.Client() as client:
-        response = client.post("https://accounts.spotify.com/api/token",
+    with httpx.AsyncClient() as client:
+        response = await client.post("https://accounts.spotify.com/api/token",
                                 headers={"Authorization": f"Basic {base_64}"} )
-        print(response)
+        return response.json()
