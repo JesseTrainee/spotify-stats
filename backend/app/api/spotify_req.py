4bd1cc11f4247ccd392c9ab048c2455496aed13f
Jesse Soares <dev.jc2021@gmail.com>
2025-06-18 16:09:20 -0300

    add new files

--- backend/app/api/spotify_req.py
+++ backend/app/api/spotify_req.py
@@ -1,7 +1,10 @@
+from typing import Annotated
+from fastapi import APIRouter, HTTPException,  Header
 import base64
-from fastapi import APIRouter
 import httpx
+from pydantic import BaseModel
 from core.config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from model.login import LoginRequest
 
 router = APIRouter()
 BASE_URL = "https://api.spotify.com/v1"
@@ -14,11 +17,45 @@ async def get_top_tracks():
         return response.json()
 
 @router.post("/login")
-async def login(username: str, password: str):
-    bytes_uft8 = f"{username}:{password}".encode("utf-8")
-    base_64 = base64.b64encode(bytes_uft8).decode("utf-8")
+async def login(data: LoginRequest):
+    # Encode client_id e client_secret em Base64 como requer o Spotify
+    bytes_utf8 = f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}".encode("utf-8")
+    base_64 = base64.b64encode(bytes_utf8).decode("utf-8")
 
-    with httpx.AsyncClient() as client:
-        response = await client.post("https://accounts.spotify.com/api/token",
-                                headers={"Authorization": f"Basic {base_64}"} )
+    headers = {
+        "Authorization": f"Basic {base_64}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    body = {
+        "grant_type": "client_credentials",
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://accounts.spotify.com/api/token",
+            headers=headers,
+            data=body
+        )
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
+    try:
         return response.json()
+    except ValueError:
+        raise HTTPException(status_code=500, detail="Resposta inválida do Spotify, não é um JSON válido.")
+
+class CommonHeaders(BaseModel):
+    host: str
+    save_data: bool
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = []
+
+@router.get("/search/")
+async def search(Authorization: Annotated[str | None, Header()], artist: str):
+    headers = {
+        "Authorization": f"{Authorization}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
